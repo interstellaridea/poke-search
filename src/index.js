605a359f0715a1e8ef401984bc8f2d2082f1f66f
Sebastian Valdez <valdez.sebastian4@gmail.com>
2018-02-24 01:51:16 -0800

    Implement middleware

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-
+import { createStore, applyMiddleware, compose } from 'redux';
+import { logger } from 'redux-logger';
+import thunk from 'redux-thunk';
 import App from './components/app';
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+const store = createStore(
+  reducers,
+  compose(
+    applyMiddleware(thunk, logger),
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+  )
+);
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider store={store}>
     <App />
   </Provider>
   , document.querySelector('.container'));
