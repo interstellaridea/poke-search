528c91a6823c4bd122a0f36b82ce16adf3054760
Sebastian Valdez <valdez.sebastian4@gmail.com>
2018-03-03 01:30:15 -0800

    revert thunk to just a returned action

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,11 +5,6 @@ export const FETCH_POKEMON = 'FETCH_POKEMON';
 const ROOT_URL = 'https://pokeapi.co/api/v2/pokemon';
 
 export function searchPokemon(pokemon) {
-  const request = axios.get(`${ROOT_URL}/${pokemon}`)
-
-  return (dispatch) => {
-    request.then( ({data}) => {
-      dispatch({ type: FETCH_POKEMON, payload: data })
-    });
-  };
+  const request = axios.get(`${ROOT_URL}/${pokemon}`);
+  return { type: FETCH_POKEMON, payload: request };
 }
